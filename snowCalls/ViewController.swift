21a5e8e64196377fe9438f18e603ca50f5991e8d
Gabriela Shaooli <gshaooli@lcs.on.ca>
2019-11-28 00:22:30 -0500

    make outlets, guard let and story board

--- snowCalls/ViewController.swift
+++ snowCalls/ViewController.swift
@@ -9,12 +9,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+// MARK:
+    //Outlets
+    @IBOutlet weak var inputInfo: UITextView!
+    
+    @IBOutlet weak var outputPhone: UITextView!
+    
+    // MARK
+    // Functions
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    // add button
+    @IBAction func convert(_ sender: Any) {
+    
+        // Input
+        inputInfo.text = ""
+        
+        // guard if no imput is provide
+        guard let input = inputInfo.text, input.count > 0 else {
+            outputPhone.text = "Please enter phone numbers to convert"
+            return
+    }
+    
+   
+        }
+        
 }
 
