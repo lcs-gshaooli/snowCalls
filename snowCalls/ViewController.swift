0e126480514ac2b435194246c624b9e903df32a4
Gabriela Shaooli <gshaooli@lcs.on.ca>
2019-11-29 21:35:00 -0500

    fixed switch statement

--- snowCalls/ViewController.swift
+++ snowCalls/ViewController.swift
@@ -15,6 +15,8 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var outputPhone: UITextView!
     
+    var builtNumber = ""
+    
     // MARK
     // Functions
     override func viewDidLoad() {
@@ -36,38 +38,45 @@ class ViewController: UIViewController {
         
         // Variable
         var outputPhone = ""
-        
-        for charcater in input {
+        if outputPhone.count < 10 {
             
-            switch charcater {
-            case "1":
-                outputPhone += "1"
-            case "A", "B", "C" :
-                outputPhone += "2"
-            case "D", "E", "F" :
-                outputPhone += "3"
-            case "G", "H", "I" :
-                outputPhone += "4"
-            case "J", "K", "L" :
-                outputPhone += "5"
-            case "M", "N", "O" :
-                outputPhone +=  "6"
-            case "P", "Q", "R", "S" :
-                outputPhone += "7"
-            case "T", "U", "V" :
-                outputPhone += "8"
-            case "W", "X", "Y", "Z" :
-                outputPhone += "9"
-            case "0":
-                outputPhone += "0"
-            default:
-                break
+            for charcater in input {
+                
+                switch charcater {
+                case "1":
+                    outputPhone += "1"
+                case "2", "A", "a", "B", "b", "C", "c":
+                    outputPhone += "2"
+                case "3", "D", "d", "E", "e", "F", "f":
+                    outputPhone += "3"
+                case "4", "G","g", "H", "h", "I", "i":
+                    outputPhone += "4"
+                case "5", "J", "j", "K", "k", "L", "l":
+                    outputPhone += "5"
+                case "6", "M", "m", "N", "n", "O", "o":
+                    outputPhone +=  "6"
+                case "7", "P", "p", "Q", "q", "R", "r", "S", "s":
+                    outputPhone += "7"
+                case "8", "T", "t", "U", "u", "V", "v" :
+                    outputPhone += "8"
+                case "9", "W", "w", "X", "x", "Y", "y", "Z", "z":
+                    outputPhone += "9"
+                case "0":
+                    outputPhone += "0"
+                case "\n":
+                    outputPhone += "\n"
+                default: outputPhone = ""
+                    break
+                }
+                
+                if outputPhone.count == 3 || outputPhone.count == 7 {
+                    outputPhone += "-"
+                    }
+                    
+                }
+                
+                print(outputPhone)
+                
             }
-            
         }
-        
-        
-        
-        
-    }
 }
